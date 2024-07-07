ec4be2b6ff5939574298c438a4f8d9b920509c55
PixelQuasar <47022046+PixelQuasar@users.noreply.github.com>
2024-07-07 19:38:35 +0500

    readme update and minor fixes

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # web-durak
+
+### .env example:
+```dotenv
+SERVER_HOST="localhost"
+SERVER_PORT="3500"
+REDIS_CONNECTION_URL="redis://localhost"
+```
--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,6 @@ mod game;
 mod server;
 mod routes;
 
-use std::collections::HashMap;
-use crate::lobby::Lobby;
-use crate::player::Player;
 use crate::server::create_app;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 use bb8_redis::{bb8, RedisConnectionManager};
@@ -16,19 +13,9 @@ use bb8_redis::{bb8, RedisConnectionManager};
 async fn main() {
     // load dotenv
     dotenv::dotenv().ok();
-
-
-    // for debug
-    tracing_subscriber::registry()
-        .with(
-            tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "example_tokio_redis=debug".into()),
-        )
-        .with(tracing_subscriber::fmt::layer())
-        .init();
-    tracing::debug!("connecting to redis");
-
-    let manager = RedisConnectionManager::new("redis://localhost").unwrap();
+    let manager = RedisConnectionManager::new(
+        dotenv::var("REDIS_CONNECTION_URL").expect("REDIS_CONNECTION_URL environment variable is empty")
+    ).unwrap();
     let redis_pool = bb8::Pool::builder().build(manager).await.unwrap();
 
     create_app(redis_pool).await;
