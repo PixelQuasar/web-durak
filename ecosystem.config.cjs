08d07a9dad4629c5865aaaed463fa1e69e22bbc3
PixelQuasar <47022046+PixelQuasar@users.noreply.github.com>
2024-08-18 23:08:47 +0500

    update ecosystem.config.js

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,14 +3,14 @@ module.exports = {
         {
             name: "web-durak-client",
             script: "npm run start",
-            watch_delay: 100000,
+            watch_delay: 10,
             ignore_watch : ["node_modules"],
             watch: false,
         },
         {
             name: "web-durak-server",
             script: "cargo run --release",
-            watch_delay: 100000,
+            watch_delay: 10,
             watch: false,
         },
     ],
